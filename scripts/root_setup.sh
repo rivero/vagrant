3f43c59e504eb34d221d9117a868b245593aedf2
Jaguar Rivero <jaguar.rivero@motorolasolutions.com>
2018-10-16 15:27:50 -0700

    Added waiting times for booting commands

--- scripts/root_setup.sh
+++ scripts/root_setup.sh
@@ -3,6 +3,9 @@
 
 set -e
 
+ADMINUSER="admin"
+USER="jrivero"
+
 # Updating and Upgrading dependencies
 sudo apt-get update -y -qq > /dev/null
 sudo apt-get upgrade -y -qq > /dev/null
@@ -15,8 +18,8 @@ sudo apt-get -y -q install linux-headers-$(uname -r) build-essential dkms nfs-co
 sudo apt-get -y -q install curl wget git tmux firefox xvfb vim
 
 # Setup sudo to allow no-password sudo for "admin"
-groupadd -r admin
-usermod -a -G admin jaguar
+groupadd -r "${ADMINUSER}"
+usermod -a -G "${ADMINUSER}" "${USER}"
 cp /etc/sudoers /etc/sudoers.orig
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
 sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
